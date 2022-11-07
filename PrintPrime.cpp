f1aa6948cbe8b2de20e3338cea7c985401fa9b72
Rajan Sharma <82056580+Immortal-codes@users.noreply.github.com>
2022-11-07 18:51:41 +0530

    Add files via upload

---
+++ PrintPrime.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    for (int i = 0; i <=50; i++)
+    {
+        if(i%2!=0&&i%3!=0)
+        {
+            cout<<i<<" ";
+        }
+    }
+    
+}
